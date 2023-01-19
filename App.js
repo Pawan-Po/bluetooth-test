eacd44ef64b02b6fe5c36a09f3a59ac47333a5cc
Pawan-Po <krpawan16.po@gmail.com>
2023-01-19 11:14:15 +0600

    ble test

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import BleManager from '@config-plugins/react-native-ble-plx';
+const bleManager = new BleManager();
+
+
 export default function App() {
+
+  bleManager.startDeviceScan([], (error, device) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(device);
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
